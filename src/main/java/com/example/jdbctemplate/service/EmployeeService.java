6a03f88997a115a3b9779214e0381e21e560a9e9
Pratiksha Singh <pratikshasingh923@example.com>
2026-09-03 11:52:27 +0530

    Add employee service for crud

---
+++ src/main/java/com/example/jdbctemplate/service/EmployeeService.java
@@ -0,0 +1,38 @@
+package com.example.jdbctemplate.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.example.jdbctemplate.entity.Employee;
+import com.example.jdbctemplate.repository.EmployeeRepository;
+import com.example.jdbctemplate.repository.EmployeeRepository2;
+
+@Service
+public class EmployeeService {
+    private final EmployeeRepository employeeRepository;
+    private final EmployeeRepository2 employeeRepository2;
+    public EmployeeService(EmployeeRepository employeeRepository,EmployeeRepository2 employeeRepository2) {
+        this.employeeRepository = employeeRepository;
+        this.employeeRepository2 = employeeRepository2;
+    }
+    public int saveEmployee(Employee employee) {
+        return employeeRepository2.saveEmployee(employee);
+    }
+    public List<Employee> getAllEmployees() {
+        return employeeRepository2.getAllEmployee();
+    }
+    public Employee getById(Long id) {
+        return employeeRepository2.getById(id);
+    }
+    public int updateEmployee(Long id, Employee employee) {
+        return employeeRepository2.updateEmployee(employee,id);
+    }
+    public int deleteEmployee(Long id) {
+        return employeeRepository2.deleteEmployee(id);
+    }
+    public int updatePatchEmployee(Long id, Employee employee) {
+        return employeeRepository2.updatePatchEmployee(employee, id);
+    }
+
+}
